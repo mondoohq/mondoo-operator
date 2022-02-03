20702725fd4ea99184b58927321f8906d7cb2d91
Christoph Hartmann <chris@lollyrock.com>
2022-02-03 14:52:00 +0100

    reverse yaml to yml for mondoo config
    Signed-off-by: Christoph Hartmann <chris@lollyrock.com>

--- controllers/mondooclient_controller.go
+++ controllers/mondooclient_controller.go
@@ -438,7 +438,7 @@ func (r *MondooClientReconciler) deamonsetForMondoo(m *v1alpha1.MondooClient, cm
 					Containers: []corev1.Container{{
 						Image:   "mondoolabs/mondoo:latest",
 						Name:    "mondoo-agent",
-						Command: []string{"mondoo", "serve", "--config", "/etc/opt/mondoo/mondoo.yaml"},
+						Command: []string{"mondoo", "serve", "--config", "/etc/opt/mondoo/mondoo.yml"},
 						VolumeMounts: []corev1.VolumeMount{
 							{
 								Name:      "root",
@@ -484,7 +484,7 @@ func (r *MondooClientReconciler) deamonsetForMondoo(m *v1alpha1.MondooClient, cm
 												},
 												Items: []corev1.KeyToPath{{
 													Key:  "inventory",
-													Path: "mondoo/inventory.yaml",
+													Path: "mondoo/inventory.yml",
 												}},
 											},
 										},
@@ -495,7 +495,7 @@ func (r *MondooClientReconciler) deamonsetForMondoo(m *v1alpha1.MondooClient, cm
 												},
 												Items: []corev1.KeyToPath{{
 													Key:  "config",
-													Path: "mondoo/mondoo.yaml",
+													Path: "mondoo/mondoo.yml",
 												}},
 											},
 										},
@@ -545,7 +545,7 @@ func (r *MondooClientReconciler) deploymentForMondoo(m *v1alpha1.MondooClient, c
 					Containers: []corev1.Container{{
 						Image:   "mondoolabs/mondoo:latest",
 						Name:    "mondoo-agent",
-						Command: []string{"mondoo", "serve", "--config", "/etc/opt/mondoo/mondoo.yaml"},
+						Command: []string{"mondoo", "serve", "--config", "/etc/opt/mondoo/mondoo.yml"},
 
 						VolumeMounts: []corev1.VolumeMount{
 							{
@@ -593,7 +593,7 @@ func (r *MondooClientReconciler) deploymentForMondoo(m *v1alpha1.MondooClient, c
 												},
 												Items: []corev1.KeyToPath{{
 													Key:  "inventory",
-													Path: "mondoo/inventory.yaml",
+													Path: "mondoo/inventory.yml",
 												}},
 											},
 										},
@@ -604,7 +604,7 @@ func (r *MondooClientReconciler) deploymentForMondoo(m *v1alpha1.MondooClient, c
 												},
 												Items: []corev1.KeyToPath{{
 													Key:  "config",
-													Path: "mondoo/mondoo.yaml",
+													Path: "mondoo/mondoo.yml",
 												}},
 											},
 										},
