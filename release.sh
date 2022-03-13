48ec0197f24253840bd3e8c0185cacbc4280911c
Christoph Hartmann <chris@lollyrock.com>
2022-03-13 14:24:35 +0100

    ⭐️ update release script to simplify the release
    Signed-off-by: Christoph Hartmann <chris@lollyrock.com>

--- release.sh
+++ release.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
-VERSION=0.0.12
-CHART_VERSION=0.1.5
+# This script updates all the versions to do a release:
+# ./release.sh 0.0.13 0.1.6
+echo "Version: $1";
+VERSION=$1
+echo "Chart-Version: $2";
+CHART_VERSION=$2
+
 make manifests
 yq -i ".appVersion = \"${VERSION}\"" charts/mondoo-operator/Chart.yaml
 yq -i ".version = \"${CHART_VERSION}\"" charts/mondoo-operator/Chart.yaml
+yq -i ".controllerManager.manager.image.tag = \"v${VERSION}\"" charts/mondoo-operator/values.yaml
 CHART_NAME=charts/mondoo-operator make helm
 make bundle IMG="ghcr.io/mondoohq/mondoo-operator:v${VERSION}" VERSION="${VERSION}"
 # TODO: update controllers/webhook-manifests.yaml
