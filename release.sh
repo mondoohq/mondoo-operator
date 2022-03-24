add4be264366cbcf65c0fd5314130ed36552e801
jensgrnb <jens.grunborg@endocode.com>
2022-03-24 14:54:26 +0100

    git add .
    Signed-off-by: jensgrnb <jens.grunborg@endocode.com>

--- release.sh
+++ release.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 # This script updates all the versions to do a release:
 # ./release.sh 0.0.13 0.1.6
-echo "Version: $1";
-VERSION=$1
-echo "Chart-Version: $2";
-CHART_VERSION=$2
+echo "previous version: $1";
+PREV_VERSION=$1
+
+echo "new version: $2";
+VERSION=$2
 
 make manifests
 yq -i ".appVersion = \"${VERSION}\"" charts/mondoo-operator/Chart.yaml
-yq -i ".version = \"${CHART_VERSION}\"" charts/mondoo-operator/Chart.yaml
+yq -i ".version = \"${VERSION}\"" charts/mondoo-operator/Chart.yaml
 yq -i ".controllerManager.manager.image.tag = \"v${VERSION}\"" charts/mondoo-operator/values.yaml
 CHART_NAME=charts/mondoo-operator make helm
 make bundle IMG="ghcr.io/mondoohq/mondoo-operator:v${VERSION}" VERSION="${VERSION}"
+yq '.spec.replaces' ./bundle/manifests/mondoo-operator.clusterserviceversion.yaml 
