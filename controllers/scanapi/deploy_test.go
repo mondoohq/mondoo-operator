bc066a5c37183d8b06767266672c9a1d62b997e1
Ivan Milchev <ivan@mondoo.com>
2022-04-19 17:10:45 +0200

    added unit tests for scan API
    Signed-off-by: Ivan Milchev <ivan@mondoo.com>

---
+++ controllers/scanapi/deploy_test.go
@@ -0,0 +1,129 @@
+package scanapi
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/suite"
+	"go.mondoo.com/mondoo-operator/tests/framework/utils"
+	"sigs.k8s.io/controller-runtime/pkg/client/fake"
+
+	mondoov1alpha1 "go.mondoo.com/mondoo-operator/api/v1alpha1"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
+	"k8s.io/utils/pointer"
+	ctrl "sigs.k8s.io/controller-runtime"
+)
+
+type DeploySuite struct {
+	suite.Suite
+	ctx    context.Context
+	scheme *runtime.Scheme
+}
+
+func (s *DeploySuite) SetupSuite() {
+	s.ctx = context.Background()
+	s.scheme = clientgoscheme.Scheme
+	s.Require().NoError(mondoov1alpha1.AddToScheme(s.scheme))
+}
+
+func (s *DeploySuite) TestDeploy_Create() {
+	ns := "test-ns"
+	auditConfig := utils.DefaultAuditConfig(ns, false, false, true)
+
+	client := fake.NewClientBuilder().WithScheme(s.scheme).Build()
+
+	s.NoError(Deploy(s.ctx, client, ns, auditConfig))
+
+	ds := &appsv1.DeploymentList{}
+	s.NoError(client.List(s.ctx, ds))
+	s.Equal(1, len(ds.Items))
+
+	d := ScanApiDeployment(ns, auditConfig)
+	d.ResourceVersion = "1" // Needed because the fake client sets it.
+	s.NoError(ctrl.SetControllerReference(&auditConfig, &d, s.scheme))
+	s.Equal(d, ds.Items[0])
+
+	ss := &corev1.ServiceList{}
+	s.NoError(client.List(s.ctx, ss))
+	s.Equal(1, len(ss.Items))
+
+	service := ScanApiService(ns)
+	service.ResourceVersion = "1" // Needed because the fake client sets it.
+	s.NoError(ctrl.SetControllerReference(&auditConfig, &service, s.scheme))
+	s.Equal(service, ss.Items[0])
+}
+
+func (s *DeploySuite) TestDeploy_Update() {
+	ns := "test-ns"
+	auditConfig := utils.DefaultAuditConfig(ns, false, false, true)
+	d := ScanApiDeployment(ns, auditConfig)
+	d.Spec.Replicas = pointer.Int32(3)
+
+	service := ScanApiService(ns)
+	service.Spec.Ports[0].Port = 1234
+
+	client := fake.NewClientBuilder().WithObjects(&d, &service).WithScheme(s.scheme).Build()
+
+	s.NoError(Deploy(s.ctx, client, ns, auditConfig))
+
+	ds := &appsv1.DeploymentList{}
+	s.NoError(client.List(s.ctx, ds))
+	s.Equal(1, len(ds.Items))
+
+	d = ScanApiDeployment(ns, auditConfig)
+	s.NoError(ctrl.SetControllerReference(&auditConfig, &d, s.scheme))
+	d.ResourceVersion = "1000" // Needed because the fake client sets it.
+
+	s.Equal(d, ds.Items[0])
+
+	ss := &corev1.ServiceList{}
+	s.NoError(client.List(s.ctx, ss))
+	s.Equal(1, len(ss.Items))
+
+	service = ScanApiService(ns)
+	s.NoError(ctrl.SetControllerReference(&auditConfig, &service, s.scheme))
+	service.ResourceVersion = "1000" // Needed because the fake client sets it.
+
+	s.Equal(service, ss.Items[0])
+}
+
+func (s *DeploySuite) TestCleanup() {
+	ns := "test-ns"
+	auditConfig := utils.DefaultAuditConfig(ns, false, false, true)
+	d := ScanApiDeployment(ns, auditConfig)
+	service := ScanApiService(ns)
+	client := fake.NewClientBuilder().WithObjects(&d, &service).Build()
+
+	s.NoError(Cleanup(s.ctx, client, ns, auditConfig))
+
+	ds := &appsv1.DeploymentList{}
+	s.NoError(client.List(s.ctx, ds))
+	s.Equal(0, len(ds.Items))
+
+	ss := &corev1.ServiceList{}
+	s.NoError(client.List(s.ctx, ss))
+	s.Equal(0, len(ss.Items))
+}
+
+func (s *DeploySuite) TestCleanup_AlreadyClean() {
+	ns := "test-ns"
+	auditConfig := utils.DefaultAuditConfig(ns, false, false, true)
+	client := fake.NewClientBuilder().Build()
+
+	s.NoError(Cleanup(s.ctx, client, ns, auditConfig))
+
+	ds := &appsv1.DeploymentList{}
+	s.NoError(client.List(s.ctx, ds))
+	s.Equal(0, len(ds.Items))
+
+	ss := &corev1.ServiceList{}
+	s.NoError(client.List(s.ctx, ss))
+	s.Equal(0, len(ss.Items))
+}
+
+func TestDeploySuite(t *testing.T) {
+	suite.Run(t, new(DeploySuite))
+}
