07f39c9aa9a2e2c6739ef125766d0ceec28e87db
Ivan Milchev <ivan@mondoo.com>
2022-04-28 16:39:46 +0200

    simplified push-virtual-tag.sh
    Signed-off-by: Ivan Milchev <ivan@mondoo.com>

--- scripts/push-virtual-tag.sh
+++ scripts/push-virtual-tag.sh
@@ -3,22 +3,20 @@ set -e
 # only exit with zero if all commands of the pipeline exit successfully
 set -o pipefail
 
-IMAGE=${REGISTRY}/${IMAGE_NAME}
-echo "Creating multi-platform virtual tag for $IMAGE..."
-echo "Tags: ${TAGS}"
+echo "Creating multi-platform virtual tag for $TAGS..."
 for tag in ${TAGS}; do
     # Create manifest to join all images under one virtual tag
-    docker manifest create -a "$IMAGE:$tag" \
-            "$IMAGE:$tag-amd64" \
-            "$IMAGE:$tag-arm64" \
-            "$IMAGE:$tag-arm" \
-    echo "Created manifest $IMAGE:$tag"
+    docker manifest create -a "$tag" \
+            "$tag-amd64" \
+            "$tag-arm64" \
+            "$tag-arm" \
+    echo "Created manifest list $tag"
 
     # Annotate to set which image is build for which CPU architecture
     for arch in ${CPU_ARCHS}; do
-        docker manifest annotate --arch "$arch" "$IMAGE:$tag" "$image:$tag-$arch"
+        docker manifest annotate --arch "$arch" "$tag" "$tag-$arch"
     done
-    echo "Pushing manifest $IMAGE:$tag..."
-    docker manifest push "$IMAGE:$tag"
-    echo "Pushed manifest $IMAGE:$tag"
+    echo "Pushing manifest list $tag..."
+    docker manifest push "$tag"
+    echo "Pushed manifest list $tag"
 done
