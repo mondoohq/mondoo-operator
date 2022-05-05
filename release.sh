a75123282025136658e58bc22f5b6f96d4a5029b
Joel Diaz <joel@mondoo.com>
2022-05-05 13:07:27 -0400

    0.2.7
    Signed-off-by: Joel Diaz <joel@mondoo.com>

--- release.sh
+++ release.sh
@@ -8,9 +8,10 @@ echo "new version: $2";
 VERSION=$2
 
 make manifests
-yq -i ".appVersion = \"${VERSION}\"" charts/mondoo-operator/Chart.yaml
-yq -i ".version = \"${VERSION}\"" charts/mondoo-operator/Chart.yaml
-yq -i ".controllerManager.manager.image.tag = \"v${VERSION}\"" charts/mondoo-operator/values.yaml
-CHART_NAME=charts/mondoo-operator make helm
+
 make bundle IMG="ghcr.io/mondoohq/mondoo-operator:v${VERSION}" VERSION="${VERSION}"
 yq -i ".spec.replaces = \"mondoo-operator.v${PREV_VERSION}\"" ./bundle/manifests/mondoo-operator.clusterserviceversion.yaml 
+
+yq -i ".appVersion = \"${VERSION}\"" charts/mondoo-operator/Chart.yaml
+yq -i ".version = \"${VERSION}\"" charts/mondoo-operator/Chart.yaml
+CHART_NAME=charts/mondoo-operator make helm
