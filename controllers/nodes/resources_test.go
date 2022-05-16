47755960f08e18572687b9cb8fa8236aea7a8d86
Ivan Milchev <ivan@mondoo.com>
2022-05-16 12:59:54 +0200

    implemented setting different inventory for node scanning jobs per node
    Signed-off-by: Ivan Milchev <ivan@mondoo.com>

--- controllers/nodes/resources_test.go
+++ controllers/nodes/resources_test.go
@@ -45,6 +45,41 @@ func TestCronJobName(t *testing.T) {
 	}
 }
 
+func TestConfigMapName(t *testing.T) {
+	rand.Seed(time.Now().UnixNano())
+	prefix := "mondoo-client"
+	tests := []struct {
+		name string
+		data func() (suffix, expected string)
+	}{
+		{
+			name: "should be prefix+base+suffix when shorter than 52 chars",
+			data: func() (suffix, expected string) {
+				base := fmt.Sprintf("%s%s", prefix, InventoryConfigMapBase)
+				suffix = RandString(52 - len(base))
+				return suffix, fmt.Sprintf("%s%s", base, suffix)
+			},
+		},
+		{
+			name: "should be prefix+base+hash when longer than 52 chars",
+			data: func() (suffix, expected string) {
+				base := fmt.Sprintf("%s%s", prefix, InventoryConfigMapBase)
+				suffix = RandString(53 - len(base))
+
+				hash := fmt.Sprintf("%x", sha256.Sum256([]byte(suffix)))
+				return suffix, fmt.Sprintf("%s%s", base, hash[:52-len(base)])
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			suffix, expected := test.data()
+			assert.Equal(t, expected, ConfigMapName(prefix, suffix))
+		})
+	}
+}
+
 var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
 
 func RandString(n int) string {
