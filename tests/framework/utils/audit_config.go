1185e166db65920b8a2e7f05f941c015bc8f76c7
Ivan Milchev <ivan@mondoo.com>
2022-07-28 17:56:08 +0200

    added the possibility to switch mondoo client version in integration tests
    Signed-off-by: Ivan Milchev <ivan@mondoo.com>

--- tests/framework/utils/audit_config.go
+++ tests/framework/utils/audit_config.go
@@ -1,20 +1,36 @@
 package utils
 
 import (
+	"os"
+
 	mondoov2 "go.mondoo.com/mondoo-operator/api/v1alpha2"
+	"go.mondoo.com/mondoo-operator/pkg/utils/mondoo"
+	"go.uber.org/zap"
 	corev1 "k8s.io/api/core/v1"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/utils/pointer"
 )
 
-const MondooClientSecret = "mondoo-client"
+const (
+	MondooClientSecret         = "mondoo-client"
+	MondooClientImageTagEnvVar = "MONDOO_CLIENT_IMAGE_TAG"
+)
+
+var mondooClientImageTag = ""
+
+func init() {
+	imageTag, ok := os.LookupEnv(MondooClientImageTagEnvVar)
+	if ok {
+		mondooClientImageTag = imageTag
+	}
+}
 
 // DefaultAuditConfigMinimal returns a new Mondoo audit config with minimal default settings to
 // make sure a test passes (e.g. setting the correct secret name). Values which have defaults are not set.
 // This means that using this function in unit tests might result in strange behavior. For unit tests use
 // DefaultAuditConfig instead.
 func DefaultAuditConfigMinimal(ns string, workloads, nodes, admission bool) mondoov2.MondooAuditConfig {
-	return mondoov2.MondooAuditConfig{
+	auditConfig := mondoov2.MondooAuditConfig{
 		ObjectMeta: v1.ObjectMeta{
 			Name:      "mondoo-client",
 			Namespace: ns,
@@ -26,6 +42,14 @@ func DefaultAuditConfigMinimal(ns string, workloads, nodes, admission bool) mond
 			Admission:            mondoov2.Admission{Enable: admission},
 		},
 	}
+
+	if mondooClientImageTag != "" {
+		auditConfig.Spec.Scanner.Image.Name = mondoo.MondooClientImage
+		auditConfig.Spec.Scanner.Image.Tag = mondooClientImageTag
+		zap.S().Infof("Using image %s:%s for mondoo-client", mondoo.MondooClientImage, mondooClientImageTag)
+	}
+
+	return auditConfig
 }
 
 // DefaultAuditConfig returns a new Mondoo audit config with some default settings to
