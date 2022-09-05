e29a42930e82891df04d8593725494c0a9a4b3b8
Christian Zunker <christian@mondoo.com>
2022-09-05 07:05:43 +0200

    Fix tf actions
    Signed-off-by: Christian Zunker <christian@mondoo.com>

--- .github/terraform/aws/outputs.tf
+++ .github/terraform/aws/outputs.tf
@@ -11,19 +11,9 @@ output "cluster_arn" {
   value       = module.eks.cluster_arn
 }
 
-output "cluster_certificate_authority_data" {
-  description = "Base64 encoded certificate data required to communicate with the cluster"
-  value       = module.eks.cluster_certificate_authority_data
-}
-
 output "cluster_endpoint" {
   description = "Endpoint for your Kubernetes API server"
   value       = module.eks.cluster_endpoint
-}
-
-output "cluster_id" {
-  description = "The name/id of the EKS cluster. Will block on cluster creation until the cluster is really ready"
-  value       = module.eks.cluster_id
 }
 
 output "cluster_status" {
