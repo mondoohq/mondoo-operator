93bed33ac4ca82a10a2c628a4cf4f89ffb0ffbea
Ivan Milchev <ivan@mondoo.com>
2022-09-09 20:34:24 +0300

    don't use default node pool for gke
    Signed-off-by: Ivan Milchev <ivan@mondoo.com>

--- .github/terraform/gke/main.tf
+++ .github/terraform/gke/main.tf
@@ -8,10 +8,21 @@ resource "google_container_cluster" "cluster" {
   name               = "mondoo-operator-tests-${random_string.suffix.result}"
   project            = var.project_id
   location           = "us-central1-a"
-  initial_node_count = 1
-
   min_master_version = var.k8s_version
-  node_version       = var.k8s_version
+
+  remove_default_node_pool = true
+  initial_node_count       = 1
+}
+
+resource "google_container_node_pool" "node_pool" {
+  name       = "mondoo-operator-pool-${random_string.suffix.result}"
+  location   = "us-central1-a"
+  cluster    = google_container_cluster.cluster.id
+  node_count = 1
+
+  node_config {
+    machine_type = "e2-standard-2"
+  }
 }
 
 data "google_client_config" "default" {}
