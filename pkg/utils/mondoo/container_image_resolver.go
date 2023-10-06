228790b6818d2f3f4844b70cb61c688712efed3a
Ivan Milchev <ivan@mondoo.com>
2023-10-06 11:46:00 +0300

    use stable v9 image for cnspec
    Signed-off-by: Ivan Milchev <ivan@mondoo.com>

--- pkg/utils/mondoo/container_image_resolver.go
+++ pkg/utils/mondoo/container_image_resolver.go
@@ -17,8 +17,8 @@ import (
 
 const (
 	CnspecImageV9              = "ghcr.io/mondoohq/mondoo-operator/cnspec"
-	CnspecTagV9                = "9.0.0-beta16-rootless"
-	OpenShiftMondooClientTagV9 = "9.0.0-beta16-ubi-rootless"
+	CnspecTagV9                = "9-rootless"
+	OpenShiftMondooClientTagV9 = "9-ubi-rootless"
 
 	CnspecImage              = "docker.io/mondoo/cnspec"
 	CnspecTag                = "8-rootless"
