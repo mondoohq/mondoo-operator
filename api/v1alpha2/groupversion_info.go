23f431ae22f82d7f60c62c06057428d7a76c6801
Christoph Hartmann <chris@lollyrock.com>
2026-01-31 16:35:11 -0800

    🧹 update copyright year (#1344)
    * 🧹 update copyright year
    
    * 🧹 pin copywright version
    
    * 🧹 update copyright headers

--- api/v1alpha2/groupversion_info.go
+++ api/v1alpha2/groupversion_info.go
@@ -1,4 +1,4 @@
-// Copyright (c) Mondoo, Inc.
+// Copyright Mondoo, Inc. 2026
 // SPDX-License-Identifier: BUSL-1.1
 
 // Package v1alpha2 contains API Schema definitions for the k8s v1alpha2 API group
